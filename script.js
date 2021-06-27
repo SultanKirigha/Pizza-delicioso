8a95fa3b9405f996d0cdefa2bf8a85d32158fd97
SultanMwangovyKirigha <sultan.kirigha@student.moringaschool.com>
2021-06-27 12:38:10 +0300

    Added an order form

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// welcome to Tomato's Pizzaria v2.0 with SASS
-const button = document.querySelector('.nav-button');
-const menu = document.querySelector('nav');
-button.addEventListener('click', () => {
-  menu.classList.toggle('visible');
+// // welcome to Tomato's Pizzaria v2.0 with SASS
+// const button = document.querySelector('.nav-button');
+// const menu = document.querySelector('nav');
+// button.addEventListener('click', () => {
+//   menu.classList.toggle('visible');
+// });
+$(".large").click(function(){
+    $("#LargeInput").show();
 });
