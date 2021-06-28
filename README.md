cf219b50aa78ea6e278e6e9768a9139e33c197d4
SultanMwangovyKirigha <sultan.kirigha@student.moringaschool.com>
2021-06-29 00:15:15 +0300

    modified my files

--- README.md
+++ README.md
@@ -4,27 +4,25 @@
 
 ## Description
 Pizza-delicioso is a web application enabling customers to order different Types of pizza with one or more toppings, then knowing the total prize and have an option to either check for delivery or not.
---------------------------------------
+
 ## Author
-Sultan Kirigha Mwangovya
---------------------------------------
+* Sultan Kirigha Mwangovya
+
 ## Setup instructions
 You are welcome to make any changes to this webpage by following this process:
 
----------------------------------------
 ## Technologies used
-HTML5
-CSS
-Javascript
-Bootstrap
-jQuery
-Known bugs
+* HTML5
+* CSS
+* Javascript
+* Bootstrap
+* jQuery
+* Known bugs
 
-----------------------------------------
 ## Contact information
-Github contact: <link>https://github.com/SultanMwangoyaKirigha</link>
-Licence and Copyright
-Copyright 2021 Sultan Kirigha Mwangovya
+* Github contact: <link>https://github.com/SultanMwangoyaKirigha</link>
+* Licence and Copyright
+* Copyright 2021 Sultan Kirigha Mwangovya
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-let clientsDetails =[];
-const details = (ev)=>{
-    ev.preventDefault();
-    let Pizza ={
-        size: document.getElementById("#exampleFormControlSelect1").value,
-        fName: document.getElementById("#firstName").value,
-        lName: document.getElementById("#lastName").value,
-        eMail: document.getElementById("#emailAddress").value,
-        pType: document.getElementById("#pizzaType").value,
-        topping: document.getElementById("#Toppings").value,
-    console.log(Pizza)
-}
-    clientsDetails.push(Pizza);
-    document.forms[0].reset();
-   
-}
+// get the the selected elements on the size option 
+// get the selected option from the pizza type
+// get the selected option from the pizza toppings
+// get the selected option from the Number of pizza 
+// create a constructor with the pizza properties
+
+//arry of Object to store the sizes and its price 
+
+//arry of Object to store the type and its price 
+
+
+//arry of Object to store the toppings and its price 
+
+//have a varible total to store the prices of the selected items
+
+//loop through each array of objects
+
+//check if the selected item is equal to that element in the array
+
+//if its equal increment/add it the total
+
+//return your total
+
+//alert the user with the infomation on selected items and the total price.
+
+let small = document.getElementById("#small");
+let medium = document.getElementById("#medium");
+let large = document.getElementById("#large");
+let Peparonni = document.getElementById("#pepperoni-btn");
+let Veggie = document.getElementById("#veggie-btn");
+let BbqBeef = document.getElementById("#bbq-beef-btn");
+let Hauweian = document.getElementById("#hawaiian-btn");
+let ChickenTikka = document.getElementById("#tikka-btn");
+
+
